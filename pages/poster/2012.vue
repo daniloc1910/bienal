fd71f048ae42adcb851818b470b549adf521bc92
Danilo Campos <daniloblk@gmail.com>
2021-07-25 12:54:13 +0200

    Update 2012.vue

--- pages/poster/2012.vue
+++ pages/poster/2012.vue
@@ -1,114 +1,127 @@
 <template>
-    <div class="container black-bg">
-
-        <div @click="$router.go(-1)" class="close-button medium-p white">Fechar</div>
-
-        <div class="id-poster grid">
-            <div class="anoautor">
-                <div class="title white ano">2012</div>
-                <div class="title white autor">30 cartazes elaborados por diferentes autores brasileiros</div>
-            </div>
-
-            <div class="description">
-                <div class="p white">Intitulada A iminência das poéticas, essa edição da Bienal adotou a metáfora da constelação como proposta curatorial e estabeleceu articulações discursivas entre passado e presente; centro e periferia; objeto e linguagem. Com grande número de obras de cada artista, a exposição privilegiou artistas latino-americanos e prestou homenagens a Arthur Bispo do Rosário e Waldemar Cordeiro. O projeto Mobile Radio montou uma estação de rádio no mezanino do pavilhão, com programação que se estendeu por todo o período da exposição.</div>
-            </div>
-            
-            <img class="poster" :src="require('~/assets/poster/12.jpg')">
-
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-2.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-3.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-4.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-5.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-6.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-7.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-8.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-9.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-10.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-11.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-12.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-13.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-14.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-15.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-16.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-17.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-18.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-19.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-20.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-21.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-22.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-23.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-24.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-25.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-26.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-27.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-28.jpg')">
-            <img class="poster-pq" :src="require('~/assets/poster/12/12-29.jpg')">
-        </div>
-
-        <div class="grid">
-            <div class="info">
-                <div class="medium-p gray">Presidente da Bienal: Heitor Martins<br>
-Curador: Luis Pérez-Oramas<br>
-Curadores-adjuntos: André Severo, Tobi Maier<br>
-Curadora-assistente: Isabela Villanueva</div>
-            </div>
-        </div>
-
+  <div class="container black-bg">
+    <div @click="$router.go(-1)" class="close-button medium-p white">
+      Fechar
     </div>
+
+    <div class="id-poster grid">
+      <div class="anoautor">
+        <div class="title white ano">2012</div>
+        <div class="title white autor">
+          30 cartazes elaborados por diferentes autores brasileiros
+        </div>
+      </div>
+
+      <div class="description">
+        <div class="p white">
+          Intitulada A iminência das poéticas, essa edição da Bienal adotou a
+          metáfora da constelação como proposta curatorial e estabeleceu
+          articulações discursivas entre passado e presente; centro e periferia;
+          objeto e linguagem. Com grande número de obras de cada artista, a
+          exposição privilegiou artistas latino-americanos e prestou homenagens
+          a Arthur Bispo do Rosário e Waldemar Cordeiro. O projeto Mobile Radio
+          montou uma estação de rádio no mezanino do pavilhão, com programação
+          que se estendeu por todo o período da exposição.
+        </div>
+      </div>
+
+      <img class="poster" :src="require('~/assets/poster/12.jpg')" />
+
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-2.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-3.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-4.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-5.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-6.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-7.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-8.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-9.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-10.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-11.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-12.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-13.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-14.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-15.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-16.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-17.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-18.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-19.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-20.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-21.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-22.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-23.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-24.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-25.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-26.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-27.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-28.jpg')" />
+      <img class="poster-pq" :src="require('~/assets/poster/12/12-29.jpg')" />
+    </div>
+
+    <div class="grid">
+      <div class="info">
+        <div class="medium-p gray">
+          Presidente da Bienal: Heitor Martins<br />
+          Curador: Luis Pérez-Oramas<br />
+          Curadores-adjuntos: André Severo, Tobi Maier<br />
+          Curadora-assistente: Isabela Villanueva
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    head() {
-        return {
-            title: '2012'
-        }
+  head() {
+    return {
+      title: '2012',
     }
+  },
 }
 </script>
 
 <style scoped>
-    .container {
-        height: 100vh;
-    }
+.container {
+  height: 100vh;
+}
 
-    .title {
-        font-family: 'Helvetica Neue Medium', Helvetica, Arial, sans-serif;
-        font-size: 80px;
-        line-height: 90%;
-        letter-spacing: -0.03em;
-    }
+.title {
+  font-family: 'Helvetica Neue Medium', Helvetica, Arial, sans-serif;
+  font-size: 80px;
+  line-height: 90%;
+  letter-spacing: -0.03em;
+}
 
-    .anoautor {
-        grid-column: 7/13;
-        padding-top: 44px;
-    }
+.anoautor {
+  grid-column: 7/13;
+  padding-top: 44px;
+}
 
-    .description {
-        grid-column: 7/13;
-        margin-top: 500px;
-        margin-bottom: 48px;
-    }
+.description {
+  grid-column: 7/13;
+  margin-top: 500px;
+  margin-bottom: 48px;
+}
 
-    .poster {
-        grid-column: 1/13;
-        margin-bottom: 48px;
-    }
+.poster {
+  grid-column: 1/13;
+  margin-bottom: 48px;
+}
 
-    .info {
-        grid-column: 9/13;
-        margin-bottom: 100px;
-        margin-top: 16px;
-    }
+.info {
+  grid-column: 9/13;
+  margin-bottom: 100px;
+  margin-top: 16px;
+}
 
-    .poster-pq {
-        grid-column: span 4;
-        margin-bottom: 32px;
-    }
+.poster-pq {
+  grid-column: span 4;
+  margin-bottom: 32px;
+}
 
-    @media (max-width: 1023px) {
-        .container {
-            height: 100%;
-        }
-    }
+@media (max-width: 1023px) {
+  .container {
+    height: 100%;
+  }
+}
 </style>
